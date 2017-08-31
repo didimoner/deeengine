2284db92bf2e45602da0d974909bf712f2f183fc
Dmitry Ibragimov <dmitry.ibragimov@devpool.com>
2017-08-31 09:56:13 +0300

    fixed bug with hud speed

--- src/modules/screens/snake/game-screen.ts
+++ src/modules/screens/snake/game-screen.ts
@@ -115,10 +115,8 @@ export class SnakeGame implements GameScreen {
     }      
     
     if (this.score - this.lastScore >= SPEED_UP_SCORE) {
-      const snakeSpeed: number = this.snake.getSpeed();
-
-      this.snake.setSpeed(snakeSpeed  + 1);
-      this.hud.setSpeed(snakeSpeed);
+      this.snake.setSpeed(this.snake.getSpeed()  + 1);
+      this.hud.setSpeed(this.snake.getSpeed());
       this.lastScore = this.score;
     }
   }
